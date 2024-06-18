e6bd297b8482d4642088acbc07eb5637e4ad5f59
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-18 07:14:33 +0545

    alert box for deleting issue

--- app/issues/[id]/DeleteIssueBtn.tsx
+++ app/issues/[id]/DeleteIssueBtn.tsx
@@ -1,14 +1,34 @@
 import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
 const DeleteIssueBtn = ({ issueId }: { issueId: string }) => {
   return (
-    <Button>
-      <TrashIcon />
-      <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure to delete this issue?
+        </AlertDialog.Description>
+        <Flex gap={"3"} mt={"3"} justify={"end"}>
+          <AlertDialog.Cancel>
+            <Button variant="soft">Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">
+              <Link href={`/issues/${issueId}/delete`}>Delete</Link>
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
