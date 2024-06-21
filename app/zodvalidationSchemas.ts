08542c9b341d69817d26fb8502d865c4b60b6c41
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-21 15:57:35 +0545

    defined schema for updating issues

--- app/zodvalidationSchemas.ts
+++ app/zodvalidationSchemas.ts
@@ -8,3 +8,9 @@ export const Issueschema = z.object({
     description: z.string().min(1, "Description is required"),
     // we have many property in prisma schema but we are using title and description bcz other have default or auto generated 
 });
+export const PatchIssueschema = z.object({
+    title: z.string().min(1, "Title is required").optional(),
+    description: z.string().min(1, "Description is required").optional(),
+    assignedToUserId: z.string().min(1, "Assigned to UserId is required").optional().nullable()
+    //using .nullable so that we can assign null value
+});
