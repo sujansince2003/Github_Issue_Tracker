e6dc1c5b064ce0ee00870126e5929f788f8b200b
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-23 07:31:01 +0545

    built API for filtering issues based on status

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,14 +3,33 @@ import prisma from "@/prisma/PrismaClient"
 import { Issueschema } from "@/app/zodvalidationSchemas";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/AuthOptions";
-import { error } from "console";
+import { Status } from "@prisma/client";
 
 
-export async function GET() {
+
+export async function GET(request: NextRequest) {
     const session = getServerSession(authOptions)
     if (!session)
         return NextResponse.json({ error: "need authorization" }, { status: 401 })
-    const newIssue = await prisma.issue.findMany()
+
+    // getting search params 
+
+    const url = new URL(request.url);
+    const filterQuery = url.searchParams.get("status")
+
+
+    // defining a function to check if a given filterQuery params is valid Status value defined in schema
+    const isValidStatus = (status: string | null): status is Status => {
+        return status === "OPEN" || status === "CLOSED" || status === "IN_PROGRESS"
+    }
+
+    const whereClause = isValidStatus(filterQuery) ? { status: filterQuery } : {}
+
+    const newIssue = await prisma.issue.findMany(
+        {
+            where: whereClause
+        }
+    )
 
     return NextResponse.json(newIssue, { status: 200 })
 }
