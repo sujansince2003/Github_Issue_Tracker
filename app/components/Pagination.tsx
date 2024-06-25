52d9521b5a72bc12361d18b94cc67753837284dc
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-06-25 23:35:03 +0545

    implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -5,6 +6,7 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 interface props {
@@ -15,16 +17,35 @@ interface props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: props) => {
+  const router = useRouter();
+  const searchparams = useSearchParams();
+
+  function changePage(page: number) {
+    const params = new URLSearchParams(searchparams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  }
+
   const pageCount = Math.ceil(itemCount / pageSize);
 
   if (pageCount <= 1) return null;
   return (
     <Flex align="center" gap="3">
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <Text>First Page</Text>
         <DoubleArrowLeftIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <Text>Previous Page</Text>
         <ChevronLeftIcon />
       </Button>
@@ -32,11 +53,21 @@ const Pagination = ({ itemCount, pageSize, currentPage }: props) => {
       <Text size={"2"}>
         Page {currentPage} of {pageCount}
       </Text>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <Text>Next Page</Text>
         <ChevronRightIcon />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <Text>Last Page</Text>
         <DoubleArrowRightIcon />
       </Button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import Image from "next/image";
+import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <div>hello</div>;
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string }; //page represent pagenumber which should be number but we are using string because we are getting from URL.we will manually convert it to number
+}) {
+  return (
+    <>
+      <div>hello</div>
+      <Pagination
+        itemCount={50}
+        pageSize={5}
+        currentPage={parseInt(searchParams.page)}
+      />
+    </>
+  );
 }
