268174d01915e27dc99bc30359543e87db68c365
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-01 18:54:42 +0545

    removed unnecessary code and imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image";
-import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
 import IssueSummary from "./IssueSummary";
 import prisma from "@/prisma/PrismaClient";
 import IssueCharts from "./IssueCharts";
 import { Flex, Grid } from "@radix-ui/themes";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { page: string }; //page represent pagenumber which should be number but we are using string because we are getting from URL.we will manually convert it to number
-}) {
+export default async function Home() {
   const openNum = await prisma.issue.count({ where: { status: "OPEN" } });
   const closedNum = await prisma.issue.count({ where: { status: "CLOSED" } });
   const inProgressNum = await prisma.issue.count({
