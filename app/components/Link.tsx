45f16a43b0268bdf2173b41f4eabbccc26c6151d
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-07-02 08:05:33 +0545

    change made to Link component

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -1,16 +1,18 @@
 // this component is build to have feature of Link from radix for styling and Link from Next for navigation
 import { Link as RadixLink } from "@radix-ui/themes";
 import NextLink from "next/link";
+import React from "react";
 
 interface Props {
   href: string;
-  children: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
-const Link = ({ href, children }: Props) => {
+const Link = ({ href, children, className }: Props) => {
   return (
     <NextLink href={href} passHref legacyBehavior>
-      <RadixLink>{children}</RadixLink>
+      <RadixLink className={className}>{children}</RadixLink>
     </NextLink>
   );
 };
